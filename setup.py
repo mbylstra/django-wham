dd200fa7a43728b3438d45c49d3fe7af52a339f9
Michael Bylstra <mbylstra@gmail.com>
2014-08-20 22:11:13 +1000

    add PyPI metadata

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django-wham',
+  packages = ['django-wham'],
+  version = '0.1',
+  description = 'Rest APIs disguised as Django ORM Models',
+  author = 'Michael Bylstra',
+  author_email = 'mbylstra@gmail.com',
+  url = 'https://github.com/mbylstra/django-wham',
+  download_url = 'https://github.com/mbylstra/django-wham/tarball/0.1',
+  keywords = ['django', 'rest', 'orm'],
+  classifiers = [],
+)
