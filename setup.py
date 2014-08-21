22cdf71e574e48c96f3dab05580f95e9577c8b17
Michael Bylstra <mbylstra@gmail.com>
2014-08-21 20:37:49 +1000

    fixes setup.py packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
 setup(
-  name = 'django-wham',
-  packages = ['wham'],
-  version = '0.1',
-  description = 'Rest APIs disguised as Django ORM Models',
-  author = 'Michael Bylstra',
-  author_email = 'mbylstra@gmail.com',
-  url = 'https://github.com/mbylstra/django-wham',
-  download_url = 'https://github.com/mbylstra/django-wham/tarball/0.1',
-  keywords = ['django', 'rest', 'orm'],
-  classifiers = [],
+    name='django-wham',
+    packages=find_packages(exclude=['wham_project']),
+    version='0.1.1',
+    description='Rest APIs disguised as Django ORM Models',
+    author='Michael Bylstra',
+    author_email='mbylstra@gmail.com',
+    url='https://github.com/mbylstra/django-wham',
+    install_requires=['django', 'requests']
 )
